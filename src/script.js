b40fa39a829c7c40f8eb823ee7a0ccbf2e1757d5
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-03-01 22:40:03 -0300

    convert routes to Fortigate

---
+++ src/script.js
@@ -0,0 +1,71 @@
+const interface = 'Tunnel0'
+
+let routesWhitoutTrash = []
+
+const specificsRouttes = [
+    {
+        address: '172.29.32.0 255.255.252.0',
+        name: 'CPD_Camaragibe'
+    },
+    {
+        address: '172.29.64.0 255.255.240.0',
+        name: 'Porto_Digital_(Setic)'
+    }
+]
+
+const routesBackupRouter = [
+    'ip route 10.239.0.0 255.255.0.0 Tunnel0 name REDE_DE_ACESSO_VPN_1',
+    'ip route 10.240.0.0 255.255.0.0 Tunnel0 name REDE_DE_ACESSO_VPN_2',
+    'ip route 172.20.0.0 255.254.0.0 Tunnel0',
+    'ip route 172.20.0.0 255.255.240.0 Tunnel0',
+    'ip route 172.20.64.0 255.255.192.0 Tunnel0',
+    'ip route 172.20.128.0 255.255.192.0 Tunnel0',
+    'ip route 172.21.0.0 255.255.0.0 Tunnel0',
+    'ip route 172.28.236.0 255.255.252.0 200.238.85.114',
+    'ip route 172.29.32.0 255.255.252.0 Tunnel0 name CPD_Camaragibe',
+    'ip route 172.29.64.0 255.255.240.0 Tunnel0 name Porto_Digital_(Setic)',
+    'ip route 177.38.35.0 255.255.255.192 Tunnel0',
+    'ip route 191.168.0.0 255.255.0.0 Tunnel0',
+    'ip route 192.168.60.0 255.255.255.0 Tunnel0',
+    'ip route 192.168.243.0 255.255.255.0 Tunnel0',
+    'ip route 192.168.251.0 255.255.255.0 Tunnel0',
+    'ip route 192.168.252.0 255.255.252.0 Tunnel0',
+    'ip route 200.216.124.0 255.255.255.0 189.80.190.17 250',
+    'ip route 200.222.75.0 255.255.255.0 189.80.190.17 250'
+]
+
+function clearingPartsOfTheRoute(route){
+    const routeCleeared = route.substring(9)
+    return routeCleeared
+}
+
+function removingRoutesTrash(route){
+    route.map( routeIndex => {
+        if(routeIndex.indexOf(interface) != -1){
+            routesWhitoutTrash.push(clearingPartsOfTheRoute(routeIndex))
+            return
+        }
+    })
+}
+
+const convertToFortigate = routesWhitoutTrash.map(route => {
+    console.log(`Rota ok: ${route}`)
+})
+
+const init = () => {
+    removingRoutesTrash(routesBackupRouter)
+
+    const convertToFortigate = routesWhitoutTrash.map(route => {
+
+        console.log(`
+            edit 0
+            set dst ${route.substring(route.indexOf(interface), -1)}
+            set device VPN_IPSEC
+            set comment "REDE_DE_ACESSO_VPN_1"
+            next
+        `)
+    })    
+    console.log(routesWhitoutTrash)
+}
+
+init()
